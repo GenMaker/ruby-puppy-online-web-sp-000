dbf92a69ae973340386b7da101f6987f49af02e4
Panela Leung <leungpanela@gmail.com>
2019-09-01 15:31:12 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,18 @@
 # Add your code here
+class Dog
+  @@name
+  @@all = []
+
+  attr_accessor :name
+
+  def initialize (name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all.each do |dog|
+      puts dog.name
+    end
+  end 
+end
